b6622272a47e7fd440b01bbf3b879201aa25bf6a
sanika <nair.sanika.pradeep@gmail.com>
2025-01-10 21:34:58 +0530

    added waitress

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from waitress import serve
+
 app=Flask(__name__)
 
 @app.route("/")
@@ -12,5 +14,5 @@ def uploader():
     return "File Uploaded Successfully"
 
     
-if (__name__=="__main__"):
-    app.run(debug=True)
+if __name__ == "__main__":
+    serve(app, host='0.0.0.0', port=8000)
